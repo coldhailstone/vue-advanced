230fae3e9cc55392010c7029a63c2658e3eb273f
coldhailstone <cksn1993@gmail.com>
2022-03-04 10:57:46 +0900

    사용자 프로필 컴포넌트 소개 및 등록

---
+++ src/components/UserProfile.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+      <section>
+            <div class="user-container">
+                <div>
+                    <i class="fa-solid fa-user"></i>
+                </div>
+                <div class="user-description">
+                    <!-- <router-link :to="`/user/${fetchedItem.user}`">
+                        {{ fetchedItem.user }}
+                    </router-link>
+                    <div class="time">
+                        {{ fetchedItem.time_ago }}
+                    </div> -->
+                </div>
+            </div>
+            <!-- <h2>{{ fetchedItem.title }}</h2> -->
+      </section>
+      <section>
+          <!-- <div v-html="fetchedItem.content"></div> -->
+      </section>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+.user-container {
+    display: flex;
+    align-items: center;
+    padding: 0.5rem;
+}
+.user-description {
+    padding-left: 8px;
+}
+.time {
+    font-size: 0.7rem;
+}
+.fa-user {
+    font-size: 2.5rem;
+}
+</style>
