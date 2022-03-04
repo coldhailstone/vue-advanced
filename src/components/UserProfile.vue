3171e104ab16b4d9d385d57655eb3bf205804626
coldhailstone <cksn1993@gmail.com>
2022-03-04 11:06:29 +0900

    사용자 컴포넌트 데이터 흐름 처리 2

--- src/components/UserProfile.vue
+++ src/components/UserProfile.vue
@@ -7,28 +7,31 @@
                 </div>
                 <div class="user-description">
                     <div>
-                        {{ userInfo.id }}
+                        {{ info.id }}
                     </div>
                     <div class="time">
-                        {{ userInfo.created }}
+                        {{ info.created }}
                     </div>
                 </div>
             </div>
-            <h2>{{ userInfo.title }}</h2>
+            <h2>{{ info.title }}</h2>
       </section>
       <section>
-          <div v-html="userInfo.about"></div>
+          <div v-html="info.about"></div>
       </section>
   </div>
 </template>
 
 <script>
 export default {
-    computed: {
-        userInfo() {
-            return this.$store.state.user;
-        }
-    },
+    props: {
+        info: Object
+    }
+    // computed: {
+    //     userInfo() {
+    //         return this.$store.state.user;
+    //     }
+    // },
 }
 </script>
 
