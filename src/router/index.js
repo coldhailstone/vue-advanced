91cb4c8579ac9448d86183fac3e4d2b578bb3920
coldhailstone <cksn1993@gmail.com>
2022-03-04 13:59:10 +0900

    하이 오더 컴포넌트(HOC) 소개 및 구현

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,9 @@
 
 import Vue from 'vue';
 import VueRouter from 'vue-router';
-import NewsView from '../views/NewsView.vue';
-import AskView from '../views/AskView.vue';
-import JobsView from '../views/JobsView.vue';
 import UserView from '../views/UserView.vue';
 import ItemView from '../views/ItemView.vue';
+import createListView from '../views/CreateListView';
 
 Vue.use(VueRouter);
 
@@ -13,9 +11,9 @@ export default new VueRouter({
     mode: 'history',
     routes: [
         { path: '/', redirect: '/news' },
-        { path: '/news', name: 'news', component: NewsView },
-        { path: '/ask', name: 'ask', component: AskView },
-        { path: '/jobs', name: 'jobs', component: JobsView },
+        { path: '/news', name: 'news', component: createListView('NewsView') },
+        { path: '/ask', name: 'ask', component: createListView('AskView') },
+        { path: '/jobs', name: 'jobs', component: createListView('JobsView') },
         { path: '/user/:id', component: UserView },
         { path: '/item/:id', component: ItemView }
     ]
